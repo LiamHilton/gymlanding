5e565bda1760f0dfa86d3eeb1e5bbef0090efcad
Liam Hilton <liamhiltoncode@gmail.com>
2025-03-25 10:27:04 +1100

    CTAsub updates for colors/background

--- src/Components/CTASUB/CTAsub.jsx
+++ src/Components/CTASUB/CTAsub.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './CTAsub.css';
+import SectionTitle from '../SectionTitle/SectionTitle';
 
 const CTAsub = () => {
   return (
     <section className="cta-section">
       <div className="cta-content">
-        <h2 className="cta-header">Ready to Start Your Fitness Journey</h2>
-        <p className="cta-paragraph">
-          Subscribe today to get our weekly updates and offers.
-          </p>
-        <a href="#" className="cta-button">Get Started</a>
+      <SectionTitle title="Start your fitness journey today" text="Choose the perfect package for your fitness journey" />
+
+        <a href="#contact" className="cta-button">Get Started</a>
       </div>
     </section>
   );
