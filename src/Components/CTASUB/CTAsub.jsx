4a1a3fb762adcc47f3a0e38154b1e0c206ea6a71
Liam Hilton <liamhiltoncode@gmail.com>
2025-03-25 16:38:23 +1100

    cta updates

--- src/Components/CTASUB/CTAsub.jsx
+++ src/Components/CTASUB/CTAsub.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './CTAsub.css';
-import SectionTitle from '../SectionTitle/SectionTitle';
 
 const CTAsub = () => {
   return (
     <section className="cta-section">
       <div className="cta-content">
-      <SectionTitle title="Start your fitness journey today" text="Choose the perfect package for your fitness journey" />
-
-        <a href="#contact" className="cta-button">Get Started</a>
+        <h2 className="cta-header">Get started today!</h2>
+        <p className="cta-paragraph">
+          Subscribe today to get our weekly updates and offers.
+          </p>
+        <a href="#" className="cta-button">Get Started</a>
       </div>
     </section>
   );
